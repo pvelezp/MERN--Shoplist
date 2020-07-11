80aa26058a5ae7d3292e75aeb8482c3447900585
Pablo Velez <pablo.velez.p@gmail.com>
2020-07-10 21:16:52 -0500

    Express API and connection to the Mongo Database

---
+++ MERN VELEZ SHOPLIST/routes/api/items.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+
+//Item Model
+
+const Item = require('../../models/Item')
+
+//@route GET api/items
+//@desc  Get all items
+
+router.get('/', (req,res) => {
+    Item.find()
+        .sort({ date: -1})
+        .then(items => res.json(items))
+      
+})
+
+
+//@route GET api/items
+//@desc  Create an item
+
+router.post('/', (req,res) => {
+    const newItem =  new Item({
+        name: req.body.name
+    })
+
+    newItem.save().then(item => res.json(item))
+      
+})
+
+router.delete('/:id', (req,res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({success: true})))    
+        .catch(err => res.status(404).json({success: false}))
+})
+
+module.exports = router
