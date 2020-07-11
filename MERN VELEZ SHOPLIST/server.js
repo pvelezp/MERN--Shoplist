76be51b38cbea12bd5c2581569bfca679a3d9514
Pablo Velez <pablo.velez.p@gmail.com>
2020-07-11 01:42:32 -0500

    Version 1.0.0

--- MERN VELEZ SHOPLIST/server.js
+++ MERN VELEZ SHOPLIST/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const items = require('./routes/api/items')
 
@@ -22,6 +23,17 @@ mongoose
     //Use routes
 
 app.use('/api/items', items)
+
+//Serve static assets if in production
+
+if(process.send.NODE_END === 'production') {
+    //Set static folfer
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000
 
